e21cb0d5105b9a1c4860b434ce1408c587f138cf
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2021-09-29 17:42:26 +0700

    🧛‍♀️ Upload Profile

--- src/pages/User/Profile/Profile.jsx
+++ src/pages/User/Profile/Profile.jsx
@@ -1,5 +1,93 @@
 import React from 'react'
-
+import * as S from './profile.style'
+import InputText from './../../../components/InputText/InputText'
+import range from 'lodash/range'
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <S.Profile>
+      <S.ProfileHeader>
+        <S.ProfileTitle>Hồ sơ của tôi</S.ProfileTitle>
+        <S.ProfileHeaderSubtitle>
+          Quản lý thông tin hồ sơ để bảo mật tài khoản
+        </S.ProfileHeaderSubtitle>
+      </S.ProfileHeader>
+      <S.ProfileInfo>
+        <S.ProfileLeft>
+          <S.InputLabel>
+            <S.InputLabelLabel>Email</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <S.InputLabelContentText>
+                vien200203@gmail.com
+              </S.InputLabelContentText>
+            </S.InputLabelContent>
+          </S.InputLabel>
+          <S.InputLabel>
+            <S.InputLabelLabel>Tên</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputText name="name" type="text" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+          <S.InputLabel>
+            <S.InputLabelLabel>Số điện thoại</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputText name="phone" type="text" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+          <S.InputLabel>
+            <S.InputLabelLabel>Địa chỉ</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputText name="address" type="text" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+          <S.InputLabel>
+            <S.InputLabelLabel>Ngày sinh</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <S.DateSelect>
+                <S.SelectDate
+                  title="Ngày"
+                  options={range(1, 32).map(item => ({
+                    name: item,
+                    value: item
+                  }))}
+                />
+                <S.SelectDate
+                  title="Tháng"
+                  options={range(0, 12).map(item => ({
+                    name: `Tháng ${item + 1}`,
+                    value: item
+                  }))}
+                />
+                <S.SelectDate
+                  title="Năm"
+                  options={range(1900, 2024).map(item => ({
+                    name: item,
+                    value: item
+                  }))}
+                />
+              </S.DateSelect>
+            </S.InputLabelContent>
+          </S.InputLabel>
+          <S.Submit>
+            <S.ButtonSubmit type="submit">Lưu</S.ButtonSubmit>
+          </S.Submit>
+        </S.ProfileLeft>
+        <S.ProfileRight>
+          <S.AvatarUploader>
+            <S.Avatar>
+              <img
+                src="https://cf.shopee.vn/file/5fa09a5073b4283d4d59fa74f3d482ba"
+                alt=""
+              />
+            </S.Avatar>
+            <S.InputFile type="file" accept=".png,.jpg,.jpeg" />
+            <S.ButtonUpload light>Chọn ảnh</S.ButtonUpload>
+            <S.AvatarUploaderTextContainer>
+              <div>Dung lượng tối đa là 1 MB</div>
+              <div>Định dạng JPG, PNG</div>
+            </S.AvatarUploaderTextContainer>
+          </S.AvatarUploader>
+        </S.ProfileRight>
+      </S.ProfileInfo>
+    </S.Profile>
+  )
 }
---
+++ src/pages/User/Profile/profile.style.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components'
+import Select from '../../../components/Select/Select'
+import { Button } from './../../../assets/styles/utils'
+
+export const Profile = styled.div`
+  background: #fff;
+  padding: 0 30px 20px;
+  box-shadow: 0 1px 2px 0 rgba(0 0 0 /2%);
+  border-radius: 2px;
+`
+export const ProfileHeader = styled.div`
+  padding: 22px 0;
+  border-bottom: 1px solid #efefef;
+`
+export const ProfileTitle = styled.div`
+  text-transform: capitalize;
+  font-size: 1.8rem;
+  font-weight: 500;
+  color: #333;
+`
+export const ProfileHeaderSubtitle = styled.div`
+  font-size: 1.4rem;
+  color: #555;
+  margin-top: 3px;
+`
+export const ProfileInfo = styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding-top: 30px;
+`
+export const ProfileLeft = styled.div`
+  padding-right: 50px;
+  flex-grow: 1;
+`
+export const ProfileRight = styled.div`
+  width: 28rem;
+  border-left: 1px solid #efefef;
+  display: flex;
+  justify-content: center;
+`
+export const InputLabel = styled.div`
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+`
+export const InputLabelLabel = styled.div`
+  width: 20%;
+  padding-top: 1rem;
+  text-align: right;
+  color: rgba(85, 85, 85, 0.8);
+  text-transform: capitalize;
+  overflow: hidden;
+`
+export const InputLabelContent = styled.div`
+  width: 80%;
+  padding-left: 2rem;
+`
+
+export const DateSelect = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const SelectDate = styled(Select)`
+  width: 32%;
+`
+export const InputLabelContentText = styled.div`
+  font-size: 1.4rem;
+  color: #333;
+  padding-top: 1rem;
+`
+
+export const Submit = styled.div`
+  margin-bottom: 60px;
+  padding-left: calc(20% + 20px);
+`
+export const ButtonSubmit = styled(Button)`
+  height: 4rem;
+  min-width: 7rem;
+`
+export const AvatarUploader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Avatar = styled.div`
+  height: 100px;
+  width: 100px;
+  margin: 20px 0;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+`
+export const InputFile = styled.input`
+  display: none;
+`
+export const ButtonUpload = styled(Button)`
+  height: 4rem;
+  padding-left: 2.5rem;
+  padding-right: 2.5rem;
+`
+export const AvatarUploaderTextContainer = styled.div`
+  margin-top: 12px;
+  > div {
+    color: #999;
+  }
+`
