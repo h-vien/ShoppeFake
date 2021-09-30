fc862834bf981cfedeedfe6f324385afeb9923fd
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2021-09-30 11:07:27 +0700

    🛒 Purchase UI

--- src/assets/styles/utils.js
+++ src/assets/styles/utils.js
@@ -44,4 +44,17 @@ export const ButtonLink = styled(Link)`
   &:hover {
     background: #f05d40;
   }
+  ${({ light }) => {
+    if (light) {
+      return `
+          background: #fff;
+          color: #555;
+          border: 1px solid rgba(0,0,0,0.09);
+          box-shadow: 0 1px 1px 0 rgb(0 0 0 /3%);
+          &:hover{
+              background-color: rgba(0,0,0,0.02);
+          }
+          `
+    }
+  }}
 `
--- src/pages/User/Purchase/Purchase.jsx
+++ src/pages/User/Purchase/Purchase.jsx
@@ -4,7 +4,9 @@ export default function Purchase() {
   return (
     <div>
       <S.PurchaseTabs>
-        <S.PurchaseTabItem to="">Tất cả</S.PurchaseTabItem>
+        <S.PurchaseTabItem className="active" to="">
+          Tất cả
+        </S.PurchaseTabItem>
         <S.PurchaseTabItem to="">Chờ xác nhận</S.PurchaseTabItem>
         <S.PurchaseTabItem to="">Chờ lấy hàng</S.PurchaseTabItem>
         <S.PurchaseTabItem to="">Đang giao</S.PurchaseTabItem>
@@ -19,8 +21,24 @@ export default function Purchase() {
                 src="https://cf.shopee.vn/file/0492dd22fe1b5d80443f42ccfc1d3a0a_tn"
                 alt=""
               />
+              <S.OrderContent>
+                <S.OrderName>
+                  Nhựa Epoxy Resin 1kg trong suốt loại cứng
+                </S.OrderName>
+                <S.OrderQuantity>x 1</S.OrderQuantity>
+              </S.OrderContent>
             </S.OrderCardDetail>
+            <S.OrderCardPrice>đ 100000</S.OrderCardPrice>
           </S.OrderCardContent>
+          <S.OrderCardButtonContainer>
+            <S.PurchaseButton light={1} to="">
+              Xem sản phẩm
+            </S.PurchaseButton>
+            <S.TotalPrice>
+              <S.TotalPriceLabel>Tổng giá tiền</S.TotalPriceLabel>
+              <S.TotalPricePrice>đ 200.000</S.TotalPricePrice>
+            </S.TotalPrice>
+          </S.OrderCardButtonContainer>
         </S.OrderCard>
       </S.PurchaseList>
     </div>
